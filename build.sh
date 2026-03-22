d3f7967f0af43237fb40eb12a0201fa109da1a00
synning360 <prideinitself+alt@gmail.com>
2026-03-22 15:53:48 -0400

    PoC commit

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
+# Copyright 2026 synning360
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+
 # this GitHub upload is a proof of concept. The build system is currently in development.
